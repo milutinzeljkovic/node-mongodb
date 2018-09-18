aedc2b505739fafaef75117141ccda855fd54729
milutinzeljkovic <milutin.zeljkovic@gmail.com>
2018-09-18 13:21:44 +0200

    authentication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express= require('express');
 var bodyParser = require('body-parser');
-
+const _ = require('lodash');
+var {authenticate} = require('./middleware/authenticate');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
@@ -11,10 +12,29 @@ var app = express();
 
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
 
-console.log(port);
+app.patch('/todos/:id',(req,res)=>{
+   var body = _.pick(req.body,['text','completed','completedAt']);
+    var id= req.params.id;
+   if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+   }
+   if(_.isBoolean(body.completed) && body.completed){
+       body.comletedAt= new Date().getTime();
+   }else{
+       body.comletedAt=null;
+       body.completed=false;
+   }
+   Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+       if(!todo){
+           return res.status(404).send();
+       }
+       res.send(todo);
+   }).catch((e)=>{
+       res.status(400).send();
+   });
 
+});
 
 app.post('/todos',(req,res)=>{
    let todo = new Todo({
@@ -27,6 +47,32 @@ app.post('/todos',(req,res)=>{
         res.status(400).send(e);
     });
 });
+
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    let user= new User(body);
+    user.save().then(()=>{
+        return user.generateAuthToken();
+       // res.send(doc);
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+
+});
+
+app.post('/users/login',(req,res)=>{
+   var body = _.pick(req.body,['email','password']);
+   User.findByCredentials(body.email,body.password).then((user)=>{
+     return user.generateAuthToken().then((token)=>{
+         res.header('x-auth',token).send(user);
+     });
+   }).catch((e)=>{
+       res.status(400).send();
+   });
+});
+
 
 app.get('/todos',(req,res)=>{
    Todo.find().then((todos)=>{
@@ -54,11 +100,29 @@ app.get('/todos/:id',(req,res)=>{
 
 
 });
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 
+});
+
+app.get('/users/me',authenticate,(req,res)=>{
+    res.send(req.user);
+});
 
 
 
 app.listen(3000,()=>{
-    console.log('Started on port 300');
+    console.log('Started on port 3000');
 });
 
